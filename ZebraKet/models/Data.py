d4d3eb36e069368f0d9d54e908e402fdd3a77cf0
TangrisJones <cleland.theo@gmail.com>
2021-07-26 14:43:21 -0400

    added binary knapsack - todo: convert to discrete

--- ZebraKet/models/Data.py
+++ ZebraKet/models/Data.py
@@ -12,14 +12,21 @@ def generate_mock_profit_data(number_products=10, number_suppliers=10):
     """
     pass
 
-def parse_profit_dataframe(data: pd.DataFrame) -> tuple[tuple[str], tuple[float]]:
+def parse_profit_dataframe(data: pd.DataFrame) -> list[list[str], list[float], list[float]]:
     """Parses the profit dataframe
 
     Keyword arguments:
-    data -- DataFrame of how much we will sell each item
+    data -- DataFrame of how profit / cost for each product. There must be a "profit" row and "cost" row in the dataframe
     
     Returns:
     Tuple of product names (str)
+    Tuple of product cost (float)
     Tuple of product prices (float)
+
+    Example input: 
+
+            | apples | bananas 
+    price   | 6.5    | 7.1
+    cost    | 2.5    | 2.4
     """
-    return tuple(data), tuple(data.iloc[0])
+    return list(data), list(data.loc['cost']), list(data.loc['price'])
--- ZebraKet/services/ClassicalOptimizer.py
+++ ZebraKet/services/ClassicalOptimizer.py
@@ -1,33 +1,57 @@
 # # File holds the class to solve various problems classically (for example: using brute force methods)
 import pandas as pd
 import numpy as np
+from itertools import combinations
 from models.data import parse_profit_dataframe
 
-def profit_optimizer(price_data: pd.DataFrame, budget: float):
+def profit_optimizer(price_data: pd.DataFrame, budget: float) -> tuple[list[int], float, float]:
     """Optimizes the profit problem classically
     
     Keyword arguments:
     price_data -- 1D DataFrame of how much we will charge per item where the columns are the item names and values are the price in floats
     budget -- Float indicating your total budget
+
+    Returns 
+    Tuple of integers indicating the solution
+    Maximum cost found
+    Maximum weight found
     """
-    products, price = parse_profit_dataframe(price_data)        
+    products, cost, price = parse_profit_dataframe(price_data)        
+
     print('products:\n', products)
     print('price:\n', price)
 
+    number_of_products = len(products)
+    price_cumulative = 0
+    cost_cumulative = 0
+    result = []
 
+    for product_index in range(1, number_of_products + 1):
+        for product_index_combinations in combinations(np.arange(number_of_products), product_index):
+            cost_combinations = [cost[i] for i in product_index_combinations]
+            price_combinations = [price[i] for i in product_index_combinations]
+            running_cost = np.sum(cost_combinations)
+            running_price = np.sum(price_combinations)
+            if running_cost <= budget and running_price > price_cumulative:
+                price_cumulative = running_price
+                cost_cumulative = running_cost
+                result = product_index_combinations
 
+    return result, cost_cumulative, price_cumulative
 
 if __name__ == "__main__":
-    import os
-    os.chdir('..')    
 
     # Define some constants
     budget = 100 # 100 dollars buget
 
     # Fake data (todo: implement this method in Data.py)
-    prices = (1.5, 1.4, 1.8, 2.1)
+    prices = (3.5, 3.4, 3.8, 6.1)
+    costs = (1.5, 1.4, 1.8, 2.1)
     items = (f'item{i}' for i in range(len(prices)))
-    fake_data = pd.DataFrame([prices], columns=items)
+    row_names = ['price', 'cost']
+    fake_data = pd.DataFrame([prices, costs], columns=items, index=row_names)
     print('Here is our generated data: \n', fake_data)
 
-    profit_optimizer(price_data=fake_data, budget=budget)
+    result, max_cost, max_price = profit_optimizer(price_data=fake_data, budget=budget)
+    print('found solution: ', max_cost, max_price)
+    print('result\n', result)
