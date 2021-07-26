881bdd69fd5dd26603aace8ed1c1ba89e9d75059
TangrisJones <cleland.theo@gmail.com>
2021-07-26 17:15:26 -0400

    better abstractions for data gen

--- ZebraKet/models/data.py
+++ ZebraKet/models/data.py
@@ -3,33 +3,50 @@ import pandas as pd
 import numpy as np
 import random
 
-def generate_mock_cost_data(number_products: int=10, number_suppliers:int=10):
-    """Generates fake data for cost of products per supplier as a pandas table where rows are suppliers and columns are products
+def generate_mock_data(number_products: int=10, number_suppliers:int=10, markup: float=2.4):
+    """Generates 2 dataframes for cost per supplier and price per item
+
+    Input Paramaters: 
+    int number_products -- Number of products to mock
+    int number_suppliers -- Number of suppliers to mock
+    float markup -- Amount of markup for each item (used when calculating the price)
+    
+    Returns: 
+    Cost Dataframe (rows = suppliers, columns = products)
+    Price Dataframe (rows = ['price'], columns = products)
     """
-    # Cost Matrix
-    # random.seed(a=1, version=2)
-    C = np.zeros((number_suppliers, number_products))
-    #cost of all items for every supplier
-    for i in range (number_products):
-        #base cost used to calculate the cost of the item at each supplier
-        c_base = random.random()*25
-        for s in range (number_suppliers):
-            #Cost of item fluctuates from c_base 
-            C[s, i] = random.randint(90,130) / 100 * c_base
-    
-    df = pd.DataFrame(C, index=[f'supplier{i}' for i in range(number_suppliers)], columns=[f'item{i}' for i in range(number_products)])
-    return df
-    
-def generate_mock_price_data(cost_dataframe: pd.DataFrame, markup: float=2.4):
-    """Generates fake data for cost of products per supplier as a pandas table where rows are suppliers and columns are products
-    
-    This uses the cost_dataframe generated from "generate_mock_cost_data" and marks the price up by multiplying by markup
-    """
-    cost = np.array(cost_dataframe)
-    P = np.amax(cost, axis=0) 
-    P = [p*markup for p in P]
-    df = pd.DataFrame([P], columns=cost_dataframe.columns, index=['price'])
-    return df
+    def generate_mock_cost_data(number_products: int=10, number_suppliers:int=10):
+        """Generates fake data for cost of products per supplier as a pandas table where rows are suppliers and columns are products
+        """
+        # Cost Matrix
+        # random.seed(a=1, version=2)
+        C = np.zeros((number_suppliers, number_products))
+        #cost of all items for every supplier
+        for i in range (number_products):
+            #base cost used to calculate the cost of the item at each supplier
+            c_base = random.random()*25
+            for s in range (number_suppliers):
+                #Cost of item fluctuates from c_base 
+                C[s, i] = random.randint(90,130) / 100 * c_base
+        
+        df = pd.DataFrame(C, index=[f'supplier{i}' for i in range(number_suppliers)], columns=[f'item{i}' for i in range(number_products)])
+        return df
+        
+    def generate_mock_price_data(cost_dataframe: pd.DataFrame, markup: float):
+        """Generates fake data for cost of products per supplier as a pandas table where rows are suppliers and columns are products
+        
+        This uses the cost_dataframe generated from "generate_mock_cost_data" and marks the price up by multiplying by markup
+        """
+        cost = np.array(cost_dataframe)
+        P = np.amax(cost, axis=0) 
+        P = [p*markup for p in P]
+        df = pd.DataFrame([P], columns=cost_dataframe.columns, index=['price'])
+        return df
+
+    cost_df = generate_mock_cost_data(number_products=number_products, number_suppliers=number_suppliers)
+    price_df = generate_mock_price_data(cost_dataframe=cost_df, markup=markup)
+
+    return cost_df, price_df
 
 def parse_profit_dataframe(data: pd.DataFrame) -> list[list[str], list[float], list[float]]:
     """Parses the profit dataframe
@@ -54,9 +71,10 @@ def parse_profit_dataframe(data: pd.DataFrame) -> list[list[str], list[float], l
     return list(data), cost, profit
 
 if __name__ == "__main__":
-    mock_cost_data = generate_mock_cost_data(20, 10)
 
-    print(mock_cost_data)
+    # Example usage
 
-    mock_price_data = generate_mock_price_data(mock_cost_data)
-    print(mock_price_data)
+    cost_dataframe, price_dataframe = generate_mock_data(20, 10)
+    print('Cost DataFrame:\n', cost_dataframe, '\n')
+
+    print('Price DataFrame:\n', price_dataframe, '\n')
