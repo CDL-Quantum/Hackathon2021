b58510a40ac6350fe95419d72a2f8757ff1c3f6a
TangrisJones <cleland.theo@gmail.com>
2021-07-27 16:19:41 -0400

    adding supplier -> profit chain link

--- ZebraKet/services/Chain.py
+++ ZebraKet/services/Chain.py
@@ -1,3 +1,6 @@
+from utils.data import read_inventory_optimization_data
+
+
 class Chain(list):
     def __init__(self) -> None:
         super().__init__()
@@ -35,37 +38,53 @@ if __name__ == "__main__":
     from models.SupplierQubo import SupplierQubo
     from utils.data import read_profit_optimization_data
     from config import standard_mock_data
-    # from neal import SimulatedAnnealingSampler
+    from neal import SimulatedAnnealingSampler
     from dwave.system import LeapHybridDQMSampler
 
     budget = 1000
     max_number_of_products = 30
-    
-    profit, cost = read_profit_optimization_data(standard_mock_data['small'])
-    sampler1 = LeapHybridDQMSampler().sample_dqm
-    sampler2 = LeapHybridDQMSampler().sample_dqm
-    
 
-    # supplier_qubo = SupplierQubo()
+    inventory_requirement, supplier_inventory = read_inventory_optimization_data(standard_mock_data['small'])
 
-    # price_qubo = ProfitQubo(sampler1, profit, cost)
-    # qubo0.define_post_process_function(lambda solution, energy: dict(profits=profit, costs=cost))
-    # # qubo0.solve()
-    # # print(qubo0.post_process)
-    # qub
-    
-    # qubo1 = ProfitQubo(sampler2)
+    # def return_
 
-    qubo0 = ProfitQubo(profit, cost)
+    qubo0 = SupplierQubo(inventory_requirement, supplier_inventory)
     qubo0.define_post_process_function(lambda solution, energy: dict(profits=profit, costs=cost))
+    sampler0 = SimulatedAnnealingSampler().sample
+    sampler0_params = dict()
 
     qubo1 = ProfitQubo()
+    sampler1 = LeapHybridDQMSampler().sample_dqm
+    sampler1_params = dict()
+
+
+    
+    # profit, cost = read_profit_optimization_data(standard_mock_data['small'])
+    # sampler1 = LeapHybridDQMSampler().sample_dqm
+    # sampler2 = LeapHybridDQMSampler().sample_dqm
+    
+
+    # # supplier_qubo = SupplierQubo()
+
+    # # price_qubo = ProfitQubo(sampler1, profit, cost)
+    # # qubo0.define_post_process_function(lambda solution, energy: dict(profits=profit, costs=cost))
+    # # # qubo0.solve()
+    # # # print(qubo0.post_process)
+    # # qub
+    
+    # # qubo1 = ProfitQubo(sampler2)
+
+    # qubo0 = ProfitQubo(profit, cost)
+    # qubo0.define_post_process_function(lambda solution, energy: dict(profits=profit, costs=cost))
+
+    # qubo1 = ProfitQubo()
 
     chain = Chain()
     chain.append(qubo0)
     chain.append(qubo1)
 
-    samplers = [sampler1, sampler2]
-    sampler_params = [dict(), dict()]
+    samplers = [sampler0, sampler1]
+    sampler_params = [sampler0_params, sampler1_params]
 
     chain.process_best(samplers, sampler_params)
+    
