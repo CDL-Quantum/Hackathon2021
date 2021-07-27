f0e09f463c20f92dcefec87288d34a4a8c5d82f0
TangrisJones <cleland.theo@gmail.com>
2021-07-27 00:53:14 -0400

    fixed data - no longer using a price csv -> it gets generated on the fly from cost csv

--- ZebraKet/config.py
+++ ZebraKet/config.py
@@ -3,20 +3,8 @@ import os
 data_dir = os.path.join('.', 'data')
 missing_product_price = -1  # put into price matrix when supplier does not have a product
 standard_mock_data = dict(
-    small = dict(
-        price = os.path.join(data_dir, 'small-price-mock.csv'),    
-        cost = os.path.join(data_dir, 'small-cost-mock.csv')
-    ),
-    medium = dict(
-        price = os.path.join(data_dir, 'medum-price-mock.csv'),    
-        cost = os.path.join(data_dir, 'medium-cost-mock.csv')
-    ),
-    large = dict(
-        price = os.path.join(data_dir, 'large-price-mock.csv'),    
-        cost = os.path.join(data_dir, 'large-cost-mock.csv')
-    ),
-    extra_large = dict(
-        price = os.path.join(data_dir, 'extra_large-price-mock.csv'),    
-        cost = os.path.join(data_dir, 'extra_large-cost-mock.csv')
-    )
+    small = os.path.join(data_dir, 'small-cost-mock.csv'),
+    medium = os.path.join(data_dir, 'medium-cost-mock.csv'),
+    large = os.path.join(data_dir, 'large-cost-mock.csv'),
+    extra_large = os.path.join(data_dir, 'extra_large-cost-mock.csv')
 )
--- ZebraKet/seed/seed_mock_data.py
+++ ZebraKet/seed/seed_mock_data.py
@@ -6,15 +6,14 @@ import os
 
 if __name__ == "__main__":
     for size in standard_mock_data:
-        price_savename = os.path.basename(standard_mock_data[size]['price'])
-        cost_savename = os.path.basename(standard_mock_data[size]['cost'])
+        cost_savename = os.path.basename(standard_mock_data[size])
         if size == 'small': 
-            generate_mock_data(20, 10, price_save_name=price_savename, cost_save_name=cost_savename)
+            generate_mock_data(20, 10, save_name=cost_savename)
         elif size == 'medium':
-            generate_mock_data(100, 40, price_save_name=price_savename, cost_save_name=cost_savename)
+            generate_mock_data(100, 40, save_name=cost_savename)
         elif size == 'large': 
-            generate_mock_data(200, 80, price_save_name=price_savename, cost_save_name=cost_savename)
+            generate_mock_data(200, 80, save_name=cost_savename)
         elif size == 'extra_large':
-            generate_mock_data(1000, 120, price_save_name=price_savename, cost_save_name=cost_savename)
+            generate_mock_data(1000, 120, save_name=cost_savename)
         else:
             raise ValueError((f'Unexpected value {size}'))
