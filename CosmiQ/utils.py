48fce19ec68505f019848d16c034fbb88c9f694d
henry <makhanov@utexas.edu>
2021-07-27 04:36:20 -0400

    added utils.py

---
+++ CosmiQ/utils.py
@@ -0,0 +1,223 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.optimize import curve_fit
+from scipy.signal import find_peaks
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import train_test_split
+
+import qiskit.pulse as pulse
+import qiskit.pulse.library as pulse_lib
+from qiskit.compiler import assemble
+from qiskit.pulse.library import Waveform
+from qiskit.tools.monitor import job_monitor
+
+from qiskit import pulse
+from qiskit.pulse.library import Gaussian
+from qiskit.test.mock import FakeValencia
+
+def get_job_data(job, average):
+    """Retrieve data from a job that has already run.
+    Args:
+        job (Job): The job whose data you want.
+        average (bool): If True, gets the data assuming data is an average.
+                        If False, gets the data assuming it is for single shots.
+    Return:
+        list: List containing job result data. 
+    """
+    job_results = job.result(timeout=120) # timeout parameter set to 120 s
+    result_data = []
+    for i in range(len(job_results.results)):
+        if average: # get avg data
+            result_data.append(job_results.get_memory(i)[qubit]*scale_factor) 
+        else: # get single data
+            result_data.append(job_results.get_memory(i)[:, qubit]*scale_factor)  
+    return result_data
+
+def get_closest_multiple_of_16(num):
+    """Compute the nearest multiple of 16. Needed because pulse enabled devices require 
+    durations which are multiples of 16 samples.
+    """
+    return (int(num) - (int(num)%16))
+    
+def create_ground_freq_sweep_program(freqs, drive_power):
+    """Builds a program that does a freq sweep by exciting the ground state. 
+    Depending on drive power this can reveal the 0->1 frequency or the 0->2 frequency. 
+    Args:
+        freqs (np.ndarray(dtype=float)): Numpy array of frequencies to sweep.
+        drive_power (float) : Value of drive amplitude.
+    Raises:
+        ValueError: Raised if use more than 75 frequencies; currently, an error will be thrown on the backend 
+                    if you try to do this.
+    Returns:
+        Qobj: Program for ground freq sweep experiment.
+    """
+    if len(freqs) > 75:
+        raise ValueError("You can only run 75 schedules at a time.")
+    
+    # print information on the sweep
+    print(f"The frequency sweep will go from {freqs[0] / GHz} GHz to {freqs[-1]/ GHz} GHz \
+using {len(freqs)} frequencies. The drive power is {drive_power}.")
+    
+    # Define the drive pulse
+    ground_sweep_drive_pulse = pulse_lib.gaussian(duration=drive_samples,
+                                                  sigma=drive_sigma,
+                                                  amp=drive_power,
+                                                  name='ground_sweep_drive_pulse')
+    # Create the base schedule
+    schedule = pulse.Schedule(name='Frequency sweep starting from ground state.')
+    
+    schedule |= pulse.Play(ground_sweep_drive_pulse, drive_chan)
+    schedule |= measure << schedule.duration
+    
+    # define frequencies for the sweep
+    schedule_freqs = [{drive_chan: freq} for freq in freqs]
+
+    # assemble the program
+    # Note: we only require a single schedule since each does the same thing;
+    # for each schedule, the LO frequency that mixes down the drive changes
+    # this enables our frequency sweep
+    ground_freq_sweep_program = assemble(schedule,
+                                         backend=backend, 
+                                         meas_level=1,
+                                         meas_return='avg',
+                                         shots=NUM_SHOTS,
+                                         schedule_los=schedule_freqs)
+    
+    return ground_freq_sweep_program
+    
+def fit_function(x_values, y_values, function, init_params):
+    """Fit a function using scipy curve_fit."""
+    fitparams, conv = curve_fit(function, x_values, y_values, init_params)
+    y_fit = function(x_values, *fitparams)
+    
+    return fitparams, y_fit
+    
+def baseline_remove(values):
+    """Center data around 0."""
+    return np.array(values) - np.mean(values)
+
+def apply_sideband(pulse, freq):
+    """Apply a sinusoidal sideband to this pulse at frequency freq.
+    Args:
+        pulse (SamplePulse): The pulse of interest.
+        freq (float): LO frequency for which we want to apply the sweep.
+    Return:
+        SamplePulse: Pulse with a sideband applied (oscillates at difference between freq and cal_qubit_freq).
+    """
+    # time goes from 0 to dt*drive_samples, sine arg of form 2*pi*f*t
+    t_samples = np.linspace(0, dt*drive_samples, drive_samples)
+    sine_pulse = np.sin(2*np.pi*(freq-cal_qubit_freq)*t_samples) # no amp for the sine
+    
+    # create sample pulse w/ sideband applied
+    # Note: need to make sq_pulse.samples real, multiply elementwise
+    sideband_pulse = Waveform(np.multiply(np.real(pulse.samples), sine_pulse), name='sideband_pulse')
+    
+    return sideband_pulse   
+
+
+def create_excited_freq_sweep_program(freqs, drive_power):
+    """Builds a program that does a freq sweep by exciting the |1> state. 
+    This allows us to obtain the 1->2 frequency. We get from the |0> to |1>
+    state via a pi pulse using the calibrated qubit frequency. To do the 
+    frequency sweep from |1> to |2>, we use a sideband method by tacking
+    a sine factor onto the sweep drive pulse.
+    Args:
+        freqs (np.ndarray(dtype=float)): Numpy array of frequencies to sweep.
+        drive_power (float) : Value of drive amplitude.
+    Raises:
+        ValueError: Thrown if use more than 75 frequencies; currently, an error will be thrown on the backend 
+                    if you try more than 75 frequencies.
+    Returns:
+        Qobj: Program for freq sweep experiment.
+    """
+    if len(freqs) > 75:
+        raise ValueError("You can only run 75 schedules at a time.")
+        
+    print(f"The frequency sweep will go from {freqs[0] / GHz} GHz to {freqs[-1]/ GHz} GHz \
+using {len(freqs)} frequencies. The drive power is {drive_power}.")
+
+    base_12_pulse = pulse_lib.gaussian(duration=drive_samples,
+                                        sigma=drive_sigma,
+                                        amp=drive_power,
+                                        name='base_12_pulse')
+    schedules = []
+    for jj, freq in enumerate(freqs):
+        
+        # add sideband to gaussian pulse
+        freq_sweep_12_pulse = apply_sideband(base_12_pulse, freq)
+        
+        # add commands to schedule
+        schedule = pulse.Schedule(name="Frequency = {}".format(freq))
+
+        # Add 0->1 pulse, freq sweep pulse and measure
+        schedule |= pulse.Play(pi_pulse_01, drive_chan)
+        schedule |= pulse.Play(freq_sweep_12_pulse, drive_chan) << schedule.duration 
+        schedule |= measure << schedule.duration # shift measurement to after drive pulses
+
+        schedules.append(schedule)
+
+    num_freqs = len(freqs)
+    
+    # draw a schedule
+    display(schedules[-1].draw(channels=[drive_chan, meas_chan], label=True, scale=1.0))
+    
+    # assemble freq sweep program 
+    # Note: LO is at cal_qubit_freq for each schedule; accounted for by sideband
+    excited_freq_sweep_program = assemble(schedules,
+                                          backend=backend, 
+                                          meas_level=1,
+                                          meas_return='avg',
+                                          shots=NUM_SHOTS,
+                                          schedule_los=[{drive_chan: cal_qubit_freq}]
+                                                         * num_freqs)
+    
+    return excited_freq_sweep_program
+
+
+def rel_maxima(freqs, output_data, height): 
+    """
+    Prints out relative maxima frequencies in output_data (can see peaks); height gives upper bound (abs val).
+    Be sure to set the height properly or the peak will be ignored!
+    Args:
+        freqs (list): frequency list
+        output_data (list): list of resulting signals
+        height (float): upper bound (abs val) on a peak
+    Returns:
+        list: List containing relative maxima frequencies
+    """
+    peaks, _ = find_peaks(output_data, height)
+    print("Freq. dips: ", freqs[peaks])
+    return freqs[peaks]
+    
+def IQ_012_plot(x_min, x_max, y_min, y_max):
+    """Helper function for plotting IQ plane for 0, 1, 2. Limits of plot given
+    as arguments."""
+    # zero data plotted in blue
+    plt.scatter(np.real(zero_data), np.imag(zero_data), 
+                    s=5, cmap='viridis', c='blue', alpha=0.5, label=r'$|0\rangle$')
+    # one data plotted in red
+    plt.scatter(np.real(one_data), np.imag(one_data), 
+                    s=5, cmap='viridis', c='red', alpha=0.5, label=r'$|1\rangle$')
+    # two data plotted in green
+    plt.scatter(np.real(two_data), np.imag(two_data), 
+                    s=5, cmap='viridis', c='green', alpha=0.5, label=r'$|2\rangle$')
+
+    # Plot a large dot for the average result of the 0, 1 and 2 states.
+    mean_zero = np.mean(zero_data) # takes mean of both real and imaginary parts
+    mean_one = np.mean(one_data)
+    mean_two = np.mean(two_data)
+    plt.scatter(np.real(mean_zero), np.imag(mean_zero), 
+                s=200, cmap='viridis', c='black',alpha=1.0)
+    plt.scatter(np.real(mean_one), np.imag(mean_one), 
+                s=200, cmap='viridis', c='black',alpha=1.0)
+    plt.scatter(np.real(mean_two), np.imag(mean_two), 
+                s=200, cmap='viridis', c='black',alpha=1.0)
+    
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min,y_max)
+    plt.legend()
+    plt.ylabel('I [a.u.]', fontsize=15)
+    plt.xlabel('Q [a.u.]', fontsize=15)
+    plt.title("0-1-2 discrimination", fontsize=15)
